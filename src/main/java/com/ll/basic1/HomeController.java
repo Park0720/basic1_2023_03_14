ccbc391af18f49cdd06fed0fc8ee8ed9e2c59947
Park0720 <ppmm98@naver.com>
2023-03-14 11:44:07 +0900

    C:/Program Files/Git/home/increase 추가

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -11,4 +11,26 @@ public class HomeController {
     public String showMain(){
         return "안녕하세요!!!";
     }
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showMain2(){
+        return "반갑습니다.";
+    }
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showMain3(){
+        return "즐거웠습니다.";
+    }
+    private int response;
+    private int call;
+    public HomeController(){
+        this.response = 0;
+        this.call = 1;
+    }
+    @GetMapping("/home/increase")
+    @ResponseBody
+
+    public String showincrease(){
+        return "요청횟수 : " + call++ + "\n" +"응답횟수 : " + response++;
+    }
 }
