b872fd723bf62c0c5eb900176196c5e4a8b39bf7
Park0720 <ppmm98@naver.com>
2023-03-15 09:31:28 +0900

    쿠키 기능

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -1,5 +1,8 @@
 package com.ll.basic1;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,14 +12,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 @Controller
 public class HomeController {
     private List<Person> people;
+    private int response;
+    private int call;
+    public HomeController(){
+        this.response = 0;
+        this.call = 1;
+        people = new ArrayList<>();
+    }
     @GetMapping("/home/main")
     @ResponseBody
     public String showMain(){
@@ -32,13 +40,7 @@ public class HomeController {
     public String showMain3(){
         return "즐거웠습니다.";
     }
-    private int response;
-    private int call;
-    public HomeController(){
-        this.response = 0;
-        this.call = 1;
-        people = new ArrayList<>();
-    }
+
     @GetMapping("/home/increase")
     @ResponseBody
 
@@ -106,6 +108,38 @@ public class HomeController {
     @ResponseBody
     public List<Person> showPeople(){
         return people;
+    }
+    @GetMapping("/home/reqAndResp")
+    @ResponseBody
+    public void showReqAndResp(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        int age = Integer.parseInt(req.getParameter("age"));
+        resp.getWriter().append("Hello, you are %d years old.".formatted(age));
+    }
+
+    @GetMapping("/home/cookie/increase")
+    @ResponseBody
+    public int showCookieIncrease(HttpServletRequest req, HttpServletResponse resp) throws IOException { // 리턴되는 int 값은 String 화 되어서 고객(브라우저)에게 전달된다.
+        int countInCookie = 0;
+
+        // 고객이 가져온 쿠폰에서 count 쿠폰을 찾고 그 쿠폰의 값을 가져온다.
+        if (req.getCookies() != null) {
+            countInCookie = Arrays.stream(req.getCookies())
+                    .filter(cookie -> cookie.getName().equals("count"))
+                    .map(Cookie::getValue)
+                    .mapToInt(Integer::parseInt)
+                    .findFirst()
+                    .orElse(0);
+        }
+
+        int newCountInCookie = countInCookie + 1;
+
+        // 고객이 가져온 count 쿠폰값에 1을 더한 쿠폰을 만들어서 고객에게 보낸다.
+        // 쉽게 말하면 브라우저(고객)에 저장되어 있는 count 쿠폰의 값을 1 증가시킨다.
+        // 이렇게 브라우저의 쿠키값을 변경하면 재방문시에 스프링부트가 다시 그 값을 받게 되어 있다.
+        resp.addCookie(new Cookie("count", newCountInCookie + ""));
+
+        // 응답 본문
+        return newCountInCookie;
     }
 }
 @AllArgsConstructor
