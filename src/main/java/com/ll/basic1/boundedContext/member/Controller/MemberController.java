9206c80a7fdc20aa8ece3413235eeb6bc2df4633
Park0720 <ppmm98@naver.com>
2023-03-16 14:32:09 +0900

    login.html

--- src/main/java/com/ll/basic1/boundedContext/member/Controller/MemberController.java
+++ src/main/java/com/ll/basic1/boundedContext/member/Controller/MemberController.java
@@ -17,23 +17,10 @@ public class MemberController {
     private final Rq rq;
 
     @GetMapping("/member/login")
-    @ResponseBody
     public String showLogin() {
-        if(rq.isLogined()){
-            return """
-                    <h1>이미 로그인 되었습니다.</h1>
-                    """.stripIndent();
-        }
-        return """
-                <h1>로그인</h1>
-                <form action="/member/doLogin">
-                <input type="text" placeholder="아이디" name ="username">
-                <input type="password" placeholder="비밀번호" name ="password">
-                <button type="submit" value="로그인">
-                </form>
-                """;
+        return "usr/member/login";
     }
-    @GetMapping("/member/dologin")
+    @GetMapping("/member/doLogin")
     @ResponseBody
     public RsData login(String username, String password) {
         if (username == null || username.trim().length() == 0) {
