b24aa80d2a06f43d648a798c6524e360ea537052
Park0720 <ppmm98@naver.com>
2023-03-17 16:53:08 +0900

    멤버를 엔(member)티티로 변경

--- src/main/java/com/ll/basic1/boundedContext/member/entity/Member.java
+++ src/main/java/com/ll/basic1/boundedContext/member/entity/Member.java
@@ -1,23 +1,35 @@
 package com.ll.basic1.boundedContext.member.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@AllArgsConstructor
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@NoArgsConstructor
 @Getter
 @ToString
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
-    private static long lastId;
-    private final long id;
-    private final String username;
-    private final String password;
-
-    static {
-        lastId = 0;
-    }
-    public Member(String username, String password){
-        this(++lastId, username, password);
-    }
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+    private String username;
+    private String password;
 }
+
